d3a1313e0c9ceee22f041849af06ab6a2ab21200
johnyREx <johnkwofie99@icloud.com>
2023-08-27 13:35:04 -0700

    monty had me jaunty

--- main.c
+++ main.c
@@ -1,90 +1,64 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "monty.h"
 
+#define MAX_LINE_SIZE 1024
+
+globales_t globalvar = {NULL, NULL, NULL};
+
 /**
- * main - Entry point of monty interpreter
- * @argc: Number of command line arguments
- * @argv: Array of command line arguments
- * Return: EXIT_SUCCESS if successful, or EXIT_FAILURE otherwise
+ * main - entry point for the CLI program
+ * @argc: count of arguments passed to the program
+ * @argv: pointer to an array of char pointers to arguments
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
  */
-
-global_var_t globalvar;
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	char *line = NULL;
-	size_t len = 0;
-	FILE *file;
+    char *token = NULL;
+    size_t line_buf_size = MAX_LINE_SIZE;
+    int line_number = 0, flag = 0, flag2 = 0;
+    stack_t *stack = NULL;
 
-	stack_t *stack = NULL;
-	unsigned int line_number = 0;
+    if (argc != 2)
+        stderr_usage();
+    globalvar.fd = fopen(argv[1], "r");
+    if (globalvar.fd == NULL)
+        stderr_fopen(argv[1]);
+    
+    globalvar.line_buf = malloc(line_buf_size);
+    if (globalvar.line_buf == NULL)
+        stderr_malloc();
+    
+    while (fgets(globalvar.line_buf, MAX_LINE_SIZE, globalvar.fd) != NULL)
+    {
+        line_number++;
+        flag = 0;
+        flag2 = 0;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		return (EXIT_FAILURE);
-	}
+        if (globalvar.line_buf[0] == '#')
+            continue;
+        
+        token = strtok(globalvar.line_buf, DELIM);
+        globalvar.token2 = strtok(NULL, DELIM);
 
-	file = fopen(argv[1], "r");
-	if (file == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		return (EXIT_FAILURE);
-	}
+        if (token == NULL)
+        {
+            flag2 = 1;
+            nop(&stack, line_number);
+        }
 
-	init_globalvars();
+        if (flag2 == 0)
+        {
+            if (token[0] == '#')
+                continue;
+        }
 
-	while (fgets(line, len, file) != NULL)
-	{
-		line_number++;
-		tokenize_input(line);
+        if (flag == 0)
+            get_builtin(token, &stack, line_number);
+    }
 
-		if (globalvar.token2 != NULL)
-		{
-			if (strcmp(globalvar.token2, "push") == 0)
-			{
-				push(&stack, line_number);
-			}
-			else if (strcmp(globalvar.token2, "pall") == 0)
-			{
-				pall(&stack, line_number);
-			}
-			else if (strcmp(globalvar.token2, "pint") == 0)
-			{
-				pint(&stack, line_number);
-			}
-			else if (strcmp(globalvar.token2, "swap") == 0)
-			{
-				swap(&stack, line_number);
-			}
-			else if (strcmp(globalvar.token2, "add") == 0)
-			{
-				add(&stack, line_number);
-			}
-			else if (strcmp(globalvar.token2, "nop") == 0)
-			{
-				nop(&stack, line_number);
-			}
-			else
-			{
-				print_unknown_opcode_error(globalvar.token2, line_number);
-				free(line);
-				free_dlistint(stack);
-				free_globalvars();
-				fclose(file);
-				return (EXIT_FAILURE);
-			}
-		}
-
-		free(globalvar.token2);
-	}
-
-	free(line);
-	free_dlistint(stack);
-	free_globalvars();
-	fclose(file);
-
-	return (EXIT_SUCCESS);
+    free_dlistint(stack);
+    free(globalvar.line_buf);
+    globalvar.line_buf = NULL;
+    fclose(globalvar.fd);
+    return EXIT_SUCCESS;
 }
